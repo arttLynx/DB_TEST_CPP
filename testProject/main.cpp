1c213082deac73089198d9cb3beea1ca7722a02b
Thomas <higher1234566@gmail.com>
2022-03-20 14:07:15 +0300

    first commit

--- testProject/main.cpp
+++ testProject/main.cpp
@@ -26,7 +26,7 @@ int main(int argc, const char * argv[]) {
     cout << "Hello, World!\n";
     vector<string> hobbies;
     hobbies.push_back("To grow a vegetable");
-    hobbies.push_back("To be against Onalnyi");
+    hobbies.push_back("To press F");
     hobbies.push_back("Пальцевать!");
     User user = User(228, "Thomas", hobbies);
     cout << "The name of user is " << user.name << ". \n" << "the age of user is" << user.age << ".\nand the hobbies of user is \n";
